174a5b136fb3586d26fe7149dc751da214ced70a
William Weiss <william@codification.org>
2019-12-02 12:53:35 -0500

    refactor: clean up formatting

--- src/S3DocumentLoader.ts
+++ src/S3DocumentLoader.ts
@@ -1,37 +1,37 @@
 import { Logger, LoggerFactory } from '@codification/cutwater-logging';
 import { S3 } from 'aws-sdk';
 import { GetObjectRequest } from 'aws-sdk/clients/s3';
-import { AppConfig } from "./AppConfig";
-import { DocumentLoader } from "./DocumentLoader";
+import { AppConfig } from './AppConfig';
+import { DocumentLoader } from './DocumentLoader';
 
 const LOG: Logger = LoggerFactory.getLogger();
 
 export class S3DocumentLoader implements DocumentLoader {
-    private readonly s3 = new S3();
-    private config: AppConfig;
+  private readonly s3 = new S3();
+  private config: AppConfig;
 
-    constructor(config: AppConfig) {
-        this.config = config;
-    }
+  constructor(config: AppConfig) {
+    this.config = config;
+  }
 
-    public loadDocument(name: string): Promise<string> {
-        const req: GetObjectRequest = {
-            Bucket: this.config.templateBucketName,
-            Key: name,
-        };
-        return new Promise((resolve, reject) => {
-            this.s3.getObject(req, (err, data) => {
-                if (err && err.statusCode !== 404) {
-                    LOG.error(`Error loading document from S3[${req.Bucket}/${name}]:`, err);
-                    reject(new Error(`Error loading document from S3: ${req.Bucket}/${name}`));
-                } else {
-                    if (data && data.Body) {
-                        resolve(data.Body.toString());
-                    } else {
-                        reject(new Error(`Document not found: ${req.Bucket}/${name}`))
-                    }
-                }
-            });
-        });
-    }
+  public loadDocument(name: string): Promise<string> {
+    const req: GetObjectRequest = {
+      Bucket: this.config.templateBucketName,
+      Key: name,
+    };
+    return new Promise((resolve, reject) => {
+      this.s3.getObject(req, (err, data) => {
+        if (err && err.statusCode !== 404) {
+          LOG.error(`Error loading document from S3[${req.Bucket}/${name}]:`, err);
+          reject(new Error(`Error loading document from S3: ${req.Bucket}/${name}`));
+        } else {
+          if (data && data.Body) {
+            resolve(data.Body.toString());
+          } else {
+            reject(new Error(`Document not found: ${req.Bucket}/${name}`));
+          }
+        }
+      });
+    });
+  }
 }
