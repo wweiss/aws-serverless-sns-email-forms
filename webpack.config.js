eb1670354a1fa474cfd8b49bf11c6bf5220d6b9c
William Weiss <william@codification.org>
2020-05-12 11:02:22 -0500

    chore: updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 const path = require("path");
-const webpackTask = require('@codification/cutwater-build-webpack').webpack;
-const isProduction = webpackTask.buildConfig.production;
+const buildConfig = require('@codification/cutwater-build-core').getConfig();
+const isProduction = buildConfig.production;
 const webpackConfiguration = {
     mode: isProduction ? 'production' : 'development',
     entry: {
-        'sns-email-forms': path.join(__dirname, webpackTask.buildConfig.libFolder, 'lambda', 'EmailFormHandler.js')
+        'sns-email-forms': path.join(__dirname, buildConfig.libES6Folder, 'lambda', 'EmailFormHandler.js')
     },
     output: {
         libraryTarget: 'umd',
-        path: path.join(__dirname, webpackTask.buildConfig.distFolder),
+        path: path.join(__dirname, buildConfig.distFolder),
         filename: `[name].js`,
     },
     externals: ['aws-sdk'],
