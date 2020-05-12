411d785974c8103339cde8ca10ae989ead9805d4
William Weiss <william@codification.org>
2020-05-12 11:02:36 -0500

    feat: add support for CORS

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -16,4 +16,16 @@ export class AppConfig {
   public static get isReCaptchaEnabled(): boolean {
     return Config.get('RECAPTCHA_ENABLED', 'false').toLocaleLowerCase() === 'true';
   }
+
+  public static get isCORSEnabled(): boolean {
+    return Config.get('CORS_ENABLED', 'false').toLocaleLowerCase() === 'true';
+  }
+
+  public static get corsHeaders(): string {
+    return Config.get('CORS_HEADERS', '*');
+  }
+
+  public static get corsOrigins(): string[] {
+    return Config.get('CORS_ORIGINS', '*').split(',');
+  }
 }
--- src/lambda/EmailFormHandler.ts
+++ src/lambda/EmailFormHandler.ts
@@ -1,5 +1,8 @@
 import { Logger, LoggerFactory } from '@codification/cutwater-logging';
-import { APIGatewayProxyEvent, Context, ProxyCallback } from 'aws-lambda';
+import middy from '@middy/core';
+import cors from '@middy/http-cors';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { AppConfig } from '../AppConfig';
 import { DocumentLoader, S3DocumentLoader } from '../document';
 import { EmailFormSender } from '../EmailFormSender';
 import { MessageFactory, MessageTransformer, ValidatingMessageFactory } from '../message';
@@ -17,30 +20,37 @@ const messageFactory: MessageFactory = new ValidatingMessageFactory(validator, n
 const messageSender: MessageSender = new SNSMessageSender();
 const sender: EmailFormSender = new EmailFormSender(messageFactory, messageSender);
 
-export const handler = (event: APIGatewayProxyEvent, context: Context, callback: ProxyCallback) => {
+const baseHandler = async (event: APIGatewayProxyEvent, context: Context) => {
   const msgCommand: MessageCommand = JSON.parse(event.body || '');
-
-  sender
-    .send(msgCommand)
-    .then(() => {
-      LOG.debug('Email sent from: ', msgCommand.from);
-      callback(null, { statusCode: 200, body: '' });
-    })
-    .catch(err => {
-      if (isValidationFailure(err)) {
-        LOG.debug('Message failed validation: ', err);
-        callback(null, {
-          statusCode: 400,
-          body: JSON.stringify(err),
-        });
-      } else {
-        LOG.error('Error sending email: ', err);
-        callback(null, {
-          statusCode: 500,
-          body: JSON.stringify({
-            errors: { internal: 'Server error delivering message.' },
-          }),
-        });
-      }
-    });
+  try {
+    await sender.send(msgCommand);
+    LOG.debug('Email sent from: ', msgCommand.from);
+    return { statusCode: 200, body: '' };
+  } catch (err) {
+    if (isValidationFailure(err)) {
+      LOG.debug('Message failed validation: ', err);
+      return {
+        statusCode: 400,
+        body: JSON.stringify(err),
+      };
+    } else {
+      LOG.error('Error sending email: ', err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          errors: { internal: 'Server error delivering message.' },
+        }),
+      };
+    }
+  }
 };
+
+export const handler = !AppConfig.isCORSEnabled
+  ? baseHandler
+  : middy(baseHandler).use(
+      cors({
+        origin: AppConfig.corsOrigins.length === 1 ? AppConfig.corsOrigins[0] : undefined,
+        origins: AppConfig.corsOrigins.length > 1 ? AppConfig.corsOrigins : undefined,
+        headers: AppConfig.corsHeaders,
+      }),
+    );
